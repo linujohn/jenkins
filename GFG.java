7b1370e484402a543b378a0db07c39cba374921b
Linu V John <linujohn1997@gmail.com>
2020-07-04 07:43:42 -0700

    Update GFG.java

--- GFG.java
+++ GFG.java
@@ -5,22 +5,15 @@ import java.util.logging.*;
 public class GFG { 
   
     public static void main(String[] args) 
-        throws SecurityException, IOException 
+       
     { 
-  
-        // Create a Logger 
-        Logger logger 
-            = Logger.getLogger( 
-                GFG.class.getName()); 
-  
-        // Set Logger level() 
-        logger.setLevel(Level.WARNING); 
-  
-        // Create a supplier<String> method 
-        Supplier<String> StrSupplier 
-            = () -> new String("WARNING WARNING WARNING"); 
-  
-        // Call warning(Supplier<String>) 
-        logger.warning(StrSupplier); 
+      
+  uncheckedGenerics();
+      
     } 
+  void uncheckedGenerics() {
+    List words = new ArrayList();
+ 
+    words.add("hello"); // this causes unchecked warning
+}
 } 
