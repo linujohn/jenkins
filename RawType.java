b590ddbed5e61d923c8a263ac875ee92be86d3c3
Linu V John <linujohn1997@gmail.com>
2020-07-05 08:37:00 -0700

    Update RawType.java

--- RawType.java
+++ RawType.java
@@ -1,15 +1,12 @@
 import java.util.*;
 public class RawType{ 
-	
+@SuppressWarnings({"rawtypes","unchecked"})	
 	public static void main(String args[])
 	{
 		
 		
 		List words= new ArrayList(); 
-		words.add("helloguys");
-		@SuppressWarnings({"rawtypes","unchecked"})
 		words.add("hello");
-		words.add("helloworld");
 
 		
 	}
